3b64871db4b1c553ed090d1ecdfe509a5ad00256
VulcanWong <wongvulcan@gmail.com>
2026-03-13 22:19:37 +0800

    lesson53

---
+++ for text.py
@@ -0,0 +1,8 @@
+while True:
+    number = input("Enter a rock, paper, scissors:")
+    if number == "rock":
+        print("scissors lose!")
+    elif number == "paper":
+        print("rock lose!")  
+    elif number == "scissors":
+        print("paper lose!")
