cd6dd060fe2634c62447c934e2850e4732e1a4e9
VulcanWong <wongvulcan@gmail.com>
2026-04-14 21:16:44 +0800

    lesson53

---
+++ homework.py
@@ -0,0 +1,12 @@
+def n(n):
+    for i in range(1, n + 1 * 2, 2*2):
+        print(i)
+    
+    m = 2
+    while(m<=n+1):
+        print("new", m)
+        m= m*2
+
+    for n  in range(0,1001):
+        print("new", n)
+n(10)
